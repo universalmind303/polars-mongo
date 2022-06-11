0a6804c5a1b7831ed5d352989bd8af32d44d8248
universalmind303 <universalmind.candy@gmail.com>
2022-06-11 17:31:52 -0500

    getting ready for crates.io publish

--- src/lib.rs
+++ src/lib.rs
@@ -196,20 +196,3 @@ pub trait MongoLazyReader {
 }
 
 impl MongoLazyReader for LazyFrame {}
-
-pub fn main() -> Result<()> {
-    let connection_str = std::env::var("POLARS_MONGO_CONNECTION_URI").unwrap();
-    let db = std::env::var("POLARS_MONGO_DB").unwrap();
-    let collection = std::env::var("POLARS_MONGO_COLLECTION").unwrap();
-
-    let df = LazyFrame::scan_mongo_collection(MongoScanOptions {
-        connection_str: &connection_str,
-        db: &db,
-        collection: &collection,
-        infer_schema_length: Some(10),
-        n_rows: None,
-    })?
-    .collect()?;
-    println!("{df}");
-    todo!()
-}
